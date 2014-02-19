c9eb1407ee7bc9fcf8b8853f06db5a6a3828de19
Matt <matt@matt.ca>
2014-02-19 11:45:54 -0800

    changed link

--- gallery.js
+++ gallery.js
@@ -1,7 +1,7 @@
 /*!
  *
  * @license Copyright (c) 2014, Matthew Smith. All rights reserved.
- * This work is subject to the terms at http://www.matt.ca/8cHk/terms-of-use.html
+ * This work is subject to the terms at http://www.matt.ca/8cHk/terms-of-use
  *
  * @author: Matthew Smith, matt@matt.ca
  *
